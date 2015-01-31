497b2c9fdf429f1e0bd03d757f002526ad028de3
Erlend Klakegg Bergheim <erlend@klakegg.net>
2015-01-31 12:31:30 +0000

    Adding bower, grunt and stuff.

---
+++ app/scripts/app.js
@@ -0,0 +1,31 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name yoApp
+ * @description
+ * # yoApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('yoApp', [
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
